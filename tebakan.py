c00942491fe40df1156acd083037aba6f74e7b49
aytidaureh <aytidaureh@gmail.com>
2017-01-20 23:47:36 +0700

    tambah tebakan

---
+++ tebakan.py
@@ -0,0 +1,15 @@
+nomor_acak = 77
+berjalan = True
+print 'tebak nomor acak dari 1 - 100'
+while berjalan:
+    tebakan = int(raw_input('Tebakan anda (bil bulat): '))
+    if tebakan == nomor_acak:
+        print 'Selamat! tebakan anda benar'
+        print 'tapi tidak ada hadiah untuk anda :('
+        berjalan = False
+    elif tebakan < nomor_acak:
+        print 'tebakan anda terlalu kecil'
+    elif tebakan > nomor_acak:
+        print "tebakan anda terlalu besar"
+else:
+    print 'selesai'
